a0ce62479715afa77be623c285ee4e97308957dd
Sam Stoelinga <sammiestoel@gmail.com>
2014-04-18 20:39:46 +0800

    Mapper working to extract features and upload to HDFS

--- src/org/tsinghua/bigdata/ImageDriver.java
+++
@@ -1,5 +0,0 @@
-package org.tsinghua.bigdata;
-
-public class ImageDriver {
-
-}
--- src/org/tsinghua/bigdata/ImageMapper.java
+++ src/org/tsinghua/bigdata/ImageMapper.java
@@ -14,31 +14,37 @@ import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
 
-
-public class ImageMapper extends MapReduceBase implements Mapper<Object, Text, Text, Text> {
-
+@SuppressWarnings("rawtypes")
+public class ImageMapper extends MapReduceBase implements
+		Mapper<Object, Text, NullOutputFormat, NullOutputFormat> {
 
 	@Override
 	public void map(Object key, Text value,
-			OutputCollector<Text, Text> arg2, Reporter reporter)
-			throws IOException {
+			OutputCollector<NullOutputFormat, NullOutputFormat> arg2,
+			Reporter reporter) throws IOException {
 		String imageFilePath = value.toString();
 		FileSystem fs = FileSystem.get(new Configuration());
-		
+
 		String namenode = "hdfs://pc201:9000";
 		FSDataInputStream fsd = fs.open(new Path(namenode + imageFilePath));
+		ImageIO.setUseCache(false);
 		BufferedImage image = ImageIO.read(fsd);
 
 		ColorLayout colorLayout = new ColorLayout();
 		colorLayout.extract(image);
 		double[] histogram = colorLayout.getDoubleHistogram();
 		reporter.setStatus("Test Sam: " + imageFilePath);
-		
+
 		String feature = imageFilePath.replaceFirst("-images", "-features");
 		feature = feature.substring(0, feature.length() - 4);
 		FSDataOutputStream fos = fs.create(new Path(namenode + feature));
@@ -49,7 +55,30 @@ public class ImageMapper extends MapReduceBase implements Mapper<Object, Text, T
 		dos.flush();
 		dos.close();
 		fos.close();
+	}
 
-	
+	/**
+	 * The actual main() method for our program; this is the "driver" for the
+	 * MapReduce job.
+	 */
+	public static void main(String[] args) {
+		JobConf conf = new JobConf(ImageMapper.class);
+		conf.setJobName("ImageMapper");
+
+		conf.setOutputKeyClass(NullOutputFormat.class);
+		conf.setOutputValueClass(NullOutputFormat.class);
+
+		FileInputFormat.addInputPath(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+		conf.setMapperClass(ImageMapper.class);
+		conf.setNumReduceTasks(0);
+
+
+		try {
+			JobClient.runJob(conf);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
